276a1079712358c11936cd412ba9400d691ff31b
Nayeef1 <mushfekmohian06@gmail.com>
2026-09-10 05:43:15 +0600

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+   int num1,num2;
+   printf("Enter first entegar:");
+   scanf("%d",&num1);
+
+   printf("Enter second entegar:");
+   scanf("%d",&num2);
+
+   printf("The numbers are %d %d",num1,num2);
+
+
+
+
+
+}
