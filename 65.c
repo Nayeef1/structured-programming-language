70bbe7707099e9ddd0751d555554cf70e9c2006a
Nayeef1 <mushfekmohian06@gmail.com>
2026-09-14 00:42:10 +0600

    Add files via upload

---
+++ 65.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a=0;
+    if(a<0){
+        printf("Negative\n");
+     } else if(a>0){
+              printf("Positive\n");}
+    else {
+        printf("Nutral\n");
+    }
+}
+
+
